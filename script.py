73e30f3692cc1db8eada60bb9f5a4113eb70da15
Михайлов Артём <artem.mikh100@gmail.com>
2025-01-13 18:52:24 +0300

    Add /weather endpoint

--- script.py
+++ script.py
@@ -1,12 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
 
+from schemas import Coordinates
+from weather_api import get_weather_data
+
 app = FastAPI()
 
 
 @app.get('/')
 def read_root():
     return {'weather_info': 'hello!'}
+
+
+@app.post("/weather")
+async def get_weather(coordinates: Coordinates):
+    return await get_weather_data(coordinates)
 
 
 if __name__ == '__main__':
