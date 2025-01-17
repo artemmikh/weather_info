7db81ccd147fa4060c60b9a82c9f596258bc853c
Михайлов Артём <artem.mikh100@gmail.com>
2025-01-17 17:51:03 +0300

    Refactor update_weather method

--- services.py
+++ services.py
@@ -12,11 +12,9 @@ async def update_weather(cities: list[City]) -> None:
         coordinates = Coordinates(lat=city.lat, lon=city.lon)
         daily_weather = await get_daily_weather(coordinates)
 
-        city.temperature_max = daily_weather["temperature_max"][0]
-        city.temperature_min = daily_weather["temperature_min"][0]
-        city.precipitation_sum = daily_weather["precipitation_sum"][0]
-        city.precipitation_hours = daily_weather["precipitation_hours"][0]
-        city.wind_speed_max = daily_weather["wind_speed_max"][0]
+        for key, value in daily_weather.items():
+            if value is not None and hasattr(city, key):
+                setattr(city, key, value[0])
 
 
 async def update_weather_for_all_cities(session: AsyncSession) -> None:
